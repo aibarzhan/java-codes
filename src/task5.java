41dfac26f3a367cb743b9d5c364cec31e656eaa9
Айбар <aibar@MacBook-Air-Ajbar.local>
2023-03-16 12:48:57 +0600

    created file

---
+++ src/task5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int sum = 0;
+        int i;
+        for (i = n1; i < n2; i++) {
+            sum = sum + i;
+        }
+        float temp = (float) sum / (n2 - n1);
+        System.out.println(temp);
+    }
+}
